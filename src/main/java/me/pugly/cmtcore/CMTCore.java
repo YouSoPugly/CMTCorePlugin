0bed72391f6e502930c3161fea16e3e5e1f040da
Pugly <dylan.m.priebe@gmail.com>
2021-03-08 19:01:00 -0600

    Initial commit

---
+++ src/main/java/me/pugly/cmtcore/CMTCore.java
@@ -0,0 +1,17 @@
+package me.pugly.cmtcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CMTCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
